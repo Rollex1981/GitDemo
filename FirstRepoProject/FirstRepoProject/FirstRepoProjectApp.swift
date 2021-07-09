7c4a1edbe4533bf3592c40cc8c2d61010bac4ee4
Roman <piskovoyroman@gmail.com>
2021-07-09 14:27:05 +0300

    Initial commit

---
+++ FirstRepoProject/FirstRepoProject/FirstRepoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstRepoProjectApp.swift
+//  FirstRepoProject
+//
+//  Created by admin on 09.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstRepoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
